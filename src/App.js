64a0d634dcc4b7f807a3c07ee6bec6d948b9cb59
cppshul <511nywSkBe6TEgG4kimV>
2020-02-19 22:44:47 +0800

    move the PureReduxCounter.jsx to folder "pureRedux"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import {Provider} from "react-redux";
 import store from "./component/sample/React-redux/store.js";
-import PureReduxCounter from "./component/sample/PureReduxCounter";
+import PureReduxCounter from "./component/sample/pureRedux/PureReduxCounter";
 import CounterComponent from "./component/sample/React-redux/CounterComponent";
 
 function App() {
