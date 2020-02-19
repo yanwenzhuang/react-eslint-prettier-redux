d06ce064f40727ef201bd3f27331aec909f6d242
cppshul <511nywSkBe6TEgG4kimV>
2020-02-19 20:13:29 +0800

    add welcome tip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <input type="button"></input>
-      </header>
+      <p>hello, please import you component in App.js, then it will appear here</p>
     </div>
   );
 }
