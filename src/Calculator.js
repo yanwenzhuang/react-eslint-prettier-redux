09e1334cbe96ed3f3a929ca8cdb2c7994ae7b8e2
Yan <850082313@qq.com>
2020-03-23 02:03:01 +0800

    wenzhuang.yan

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Calculator.css";
-import store from "./store";
+import {connect} from "react-redux";
 
 const funs = ["+", "-", "*", "/", "c", "="];
 const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -44,65 +44,55 @@ function Number(props) {
 }
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState().reducer;
-    this.numClick = this.numClick.bind(this);
-    this.actionClick = this.actionClick.bind(this);
-    // store.subscribe(() => {
-    //   this.setState(store.getState());
-    // });
-  }
-
-  componentDidMount() {
-    store.subscribe(() => {
-      this.setState(store.getState().reducer);
-    });
-  }
-
   render() {
+    const {inputValue, result, actionClick, numClick} = this.props;
     return (
       <div className="wrap">
-        <input value={this.state.inputValue} readOnly />
-        <div className="result">{this.state.result}</div>
-        <Action funs={funs} click={this.actionClick} />
-        <Number nums={nums} click={this.numClick} />
+        <input value={inputValue} readOnly />
+        <div className="result">{result}</div>
+        <Action funs={funs} click={actionClick} />
+        <Number nums={nums} click={numClick} />
       </div>
     );
   }
-
-  numClick(e) {
-    const action = {
-      type: "input_number_add",
-      value: e,
-    };
-    store.dispatch(action);
-    // const dispatchAddNumber = bindActionCreators(action, store.dispatch);
-    // dispatchAddNumber();
-  }
-
-  actionClick(e) {
-    if (e === "c") {
-      const action = {
-        type: "clear_input_value",
-      };
-      store.dispatch(action);
-      return;
-    }
-
-    if ("=" === e) {
-      const action = {
-        type: "action_number_result",
-      };
-      store.dispatch(action);
-      return;
-    }
-    const action = {
-      type: "input_number_add",
-      value: e,
-    };
-    store.dispatch(action);
-  }
 }
 
-export default Calculator;
+function mapStateToProps(state) {
+  return {
+    inputValue: state.reducer.inputValue,
+    result: state.reducer.result,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    numClick(e) {
+      const action = {
+        type: "input_number_add",
+        value: e,
+      };
+      dispatch(action);
+    },
+    actionClick(e) {
+      if (e === "c") {
+        const action = {
+          type: "clear_input_value",
+        };
+        dispatch(action);
+        return;
+      }
+      if ("=" === e) {
+        const action = {
+          type: "action_number_result",
+        };
+        dispatch(action);
+        return;
+      }
+      const action = {
+        type: "input_number_add",
+        value: e,
+      };
+      dispatch(action);
+    },
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Calculator from "./Calculator";
 import * as serviceWorker from "./serviceWorker";
+import {Provider} from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+const App = (
+  <Provider store={store}>
+    <Calculator />
+  </Provider>
+);
+
+ReactDOM.render(App, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
